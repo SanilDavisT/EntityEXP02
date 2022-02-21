484551994a2f0d8c69bf444607a2e465338c51f3
Sanil Davis <sanil.davis@speehive.com>
2022-02-21 18:23:49 +0530

    Template

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using EntityEXP02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityEXP02.Controllers
+{
+    public class AdminController : Controller
+    {
+
+
+        ShopDbContext _context;
+        
+
+        public AdminController(ShopDbContext context)
+        {
+            
+            _context = context;
+
+
+        }
+
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        //public IActionResult Index2()
+        //{
+        //    return View();
+        //}
+        //public IActionResult Index3()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index4()
+        {
+
+
+            var result = _context.Shops.ToList();
+            return View(result);
+        }
+
+    }
+}
